c03cf4d27ed8945d08d0a2666758efa0d20339c5
betselotT <betselottesfa9@gmail.com>
2025-05-13 15:08:47 +0300

    working auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,18 +4,20 @@ import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
 import { isAuthenticated } from "@/lib/actions/auth.action";
+import SignOutButton from "@/components/sign-out-button";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) redirect("/sign-in");
 
   return (
-    <div className="root-layout">
-      <nav>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">Interview Prep</h2>
+    <div className="flex mx-auto max-w-7xl flex-col gap-12 my-12 px-16 max-sm:px-4 max-sm:my-8">
+      <nav className="flex items-center p-10 gap-400">
+        <Link href="/" className="flex items-center">
+          <Image src="/logo.png" alt="Tick Logo" width={60} height={60} />
+          <h2 className="text-primary-100">Tick</h2>
         </Link>
+        <SignOutButton />
       </nav>
 
       {children}
